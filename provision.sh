aeb9eaf912cab6cfd4ca2b848ac8fa77d3baf11d
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2015-12-09 23:59:24 +0900

    Add esxui

--- provision.sh
+++ provision.sh
@@ -3,3 +3,5 @@
 cat <<EOF > /etc/motd
 Welcome to VMware ESXi $(uname -r), up'd by Vagrant $(cat /vagrant/version.txt)
 EOF
+
+esxcli software vib install -v http://download3.vmware.com/software/vmw-tools/esxui/esxui-signed.vib
